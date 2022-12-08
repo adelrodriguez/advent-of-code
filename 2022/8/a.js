912120869f0e1b400d33941b5e72185a416bc362
Adel Rodríguez <hello@adelrodriguez.com>
2022-12-08 02:24:25 -0400

    2022 - day 8

---
+++ 2022/8/a.js
@@ -0,0 +1,72 @@
+const __dirname = new URL('.', import.meta.url).pathname;
+const input = await Deno.readTextFile(__dirname + '/input.txt');
+const trees = input.split(/\n/).map((line) => line.split('').map(Number));
+
+let visible = 0;
+
+trees.forEach((row, rowIndex) => {
+  row.forEach((tree, columnIndex) => {
+    // We can skip rows 0 and length - 1 since they are all visible. We can also
+    // skip columns 0 and length - 1
+    if (
+      rowIndex === 0 ||
+      rowIndex === trees.length - 1 ||
+      columnIndex === 0 ||
+      columnIndex === row.length - 1
+    ) {
+      visible++;
+      return;
+    }
+
+    if (
+      checkUp(tree, rowIndex, columnIndex) ||
+      checkDown(tree, rowIndex, columnIndex) ||
+      checkLeft(tree, rowIndex, columnIndex) ||
+      checkRight(tree, rowIndex, columnIndex)
+    ) {
+      visible++;
+    }
+  });
+});
+
+function checkUp(tree, row, column) {
+  for (let i = row - 1; i >= 0; i--) {
+    if (trees[i][column] >= tree) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkDown(tree, row, column) {
+  for (let i = row + 1; i < trees.length; i++) {
+    if (trees[i][column] >= tree) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkRight(tree, row, column) {
+  for (let i = column + 1; i < trees[row].length; i++) {
+    if (trees[row][i] >= tree) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkLeft(tree, row, column) {
+  for (let i = column - 1; i >= 0; i--) {
+    if (trees[row][i] >= tree) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(visible);
---
+++ 2022/8/b.js
@@ -0,0 +1,87 @@
+const __dirname = new URL('.', import.meta.url).pathname;
+const input = await Deno.readTextFile(__dirname + '/input.txt');
+const trees = input.split(/\n/).map((line) => line.split('').map(Number));
+
+const scenicScores = [];
+
+trees.forEach((row, rowIndex) => {
+  row.forEach((tree, columnIndex) => {
+    // We can skip rows 0 and length - 1 since they all have a 0 edge. We can also
+    // skip columns 0 and length - 1
+    if (
+      rowIndex === 0 ||
+      rowIndex === trees.length - 1 ||
+      columnIndex === 0 ||
+      columnIndex === row.length - 1
+    ) {
+      return;
+    }
+
+    const scoreUp = checkUp(tree, rowIndex, columnIndex);
+    const scoreDown = checkDown(tree, rowIndex, columnIndex);
+    const scoreLeft = checkLeft(tree, rowIndex, columnIndex);
+    const scoreRight = checkRight(tree, rowIndex, columnIndex);
+
+    const score = scoreUp * scoreDown * scoreLeft * scoreRight;
+
+    scenicScores.push(score);
+  });
+});
+
+function checkUp(tree, row, column) {
+  let score = 0;
+
+  for (let i = row - 1; i >= 0; i--) {
+    score++;
+
+    if (trees[i][column] >= tree) {
+      return score;
+    }
+  }
+
+  return score;
+}
+
+function checkDown(tree, row, column) {
+  let score = 0;
+
+  for (let i = row + 1; i < trees.length; i++) {
+    score++;
+
+    if (trees[i][column] >= tree) {
+      return score;
+    }
+  }
+
+  return score;
+}
+
+function checkRight(tree, row, column) {
+  let score = 0;
+
+  for (let i = column + 1; i < trees[row].length; i++) {
+    score++;
+
+    if (trees[row][i] >= tree) {
+      return score;
+    }
+  }
+
+  return score;
+}
+
+function checkLeft(tree, row, column) {
+  let score = 0;
+
+  for (let i = column - 1; i >= 0; i--) {
+    score++;
+
+    if (trees[row][i] >= tree) {
+      return score;
+    }
+  }
+
+  return score;
+}
+
+console.log(scenicScores.sort((a, b) => b - a)[0]);
